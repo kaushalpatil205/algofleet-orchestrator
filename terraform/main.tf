75ef4be6aa6131f2223b273a2e2e309c326722d9
Kaushal Patil <patilkaushal89@gmail.com>
2026-08-26 11:47:11 +0530

    fix: Upgrade EKS cluster_version to 1.33 to avoid AWS Extended Support fees

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "tradops-eks"
-  cluster_version = "1.31"
+  cluster_version = "1.33"
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
