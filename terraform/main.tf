4ba43bc01c2e9186f016976ffeb7f5f4039d6980
Kaushal Patil <patilkaushal89@gmail.com>
2026-08-28 17:37:48 +0530

    fix: Add aud condition to GitHub OIDC trust policy to fix AssumeRoleWithWebIdentity error

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,6 +90,11 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
     condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values   = [
